8be2e185d5b7b8d2390336a5c1b8803aa40961dd
manuel89898989 <manuel.ascari@gmail.com>
2025-05-18 20:52:08 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Diretta Player</title>
+      </Head>
+      <main>
+        <h1>Benvenuto su Diretta Player</h1>
+        <p>App per analisi e pronostici su marcatori, assist e ammoniti.</p>
+      </main>
+    </>
+  );
+}
